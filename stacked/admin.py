d37fa35d944167cc3c949bfb704a1d7c8a6cf8f3
Eduardo Rivas <e@jerivas.com>
2016-05-18 23:25:08 -0600

    Add models and admin to showcase inline admins.

---
+++ stacked/admin.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from mezzanine.core.admin import StackedDynamicInlineAdmin, TabularDynamicInlineAdmin
+
+from .models import Family, Pet, FamilyMember
+
+
+class FamilyMemberInlineAdmin(StackedDynamicInlineAdmin):
+    model = FamilyMember
+
+
+class PetInlineAdmin(TabularDynamicInlineAdmin):
+    model = Pet
+
+
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin):
+    inlines = [FamilyMemberInlineAdmin, PetInlineAdmin]
---
+++ stacked/models.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+from mezzanine.core.models import Orderable
+
+
+@python_2_unicode_compatible
+class Family(models.Model):
+    title = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "family"
+        verbose_name_plural = "families"
+
+    def __str__(self):
+        return self.title
+
+
+@python_2_unicode_compatible
+class FamilyMember(Orderable):
+    family = models.ForeignKey(Family, related_name="members")
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Pet(Orderable):
+    family = models.ForeignKey(Family, related_name="pets")
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
